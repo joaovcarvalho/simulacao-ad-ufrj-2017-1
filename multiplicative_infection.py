688b8873b9b509df668b6c67bac3e56a5016235a
Guilherme <gobbi9@live.com>
2017-07-11 17:33:17 -0300

    Opaaa sugestao do elias

--- multiplicative_infection.py
+++ multiplicative_infection.py
@@ -1,13 +1,16 @@
 from math import log, inf
 from random import random
+from pprint import PrettyPrinter
 
-MU = 5 # decontamination/healing rate
-GAMMA = 1.1 # internal infection rate
-LAMBDA = 2 # external infection rate
+pp = PrettyPrinter(indent=4)
 
 N = 20 # population size
 
-EXPERIMENTS = 10 # number of snapshots of the system
+MU = 1 # decontamination/healing rate
+GAMMA = 0.6 # internal infection rate
+LAMBDA = 10/N # external infection rate
+
+EXPERIMENTS = 15 # number of snapshots of the system
 
 states = ['S', 'I']
 
@@ -32,11 +35,6 @@ class State:
     def clone(self):
         return State(self.origin, self.destiny, self.dt)
 
-# creates N individuals at the 'S'(0) state, who can go to the 'I'(1), but initially are staying "forever" at 'S'
-# in this case, the destiny and dt attributes of the initial population do not matter,
-# since they are randomly initialized afterwards
-state_nodes = [State(0,1,inf) for i in range(N)] # 0: 'S', 1: 'I'
-
 # exponentially distributed random number generator with rate 'rate'
 def inverse_exp(rate):
     return -log(1-random())/rate
@@ -58,53 +56,66 @@ def get_number_of_infected(state_nodes):
     return count
 
 
-# critical part of the simulation and it's not generic at all. This part has to be completely changed if, for example,
-# a new state node were to be added to the simulation model
-snapshots = []
-for exp in range(EXPERIMENTS):
-    for i in range(N):
-        node = state_nodes[i]
-        # sets the origin from the second node and so forth. Since there are only two nodes, the origin of the current
-        # node is always going to be the destiny of the previous node
-        if exp > 0:
-            node.origin = snapshots[exp - 1][i].destiny
 
-        # if you are at state 'S'(0) you can only go to state 'I'(1)
-        if node.origin == 0:
-            node.destiny = 1
-            node.dt = inverse_exp_n(LAMBDA, GAMMA, get_number_of_infected(state_nodes))
-        # if you are at state 'I'(1) you can only go to state 'S'(0)
-        elif node.origin == 1:
-            node.destiny = 0
-            node.dt = inverse_exp(MU)
+def simulation(N,GAMMA, MU, LAMBDA0, EXPERIMENTS):
+    LAMBDA = LAMBDA0/N
+    # critical part of the simulation and it's not generic at all. This part has to be completely changed if, for example,
+    # a new state node were to be added to the simulation model
+    snapshots = []
+    for exp in range(EXPERIMENTS):
+        # creates N individuals at the 'S'(0) state, who can go to the 'I'(1), but initially are staying "forever" at 'S'
+        # in this case, the destiny and dt attributes of the initial population do not matter,
+        # since they are randomly initialized afterwards
+        state_nodes = [State(0,1,0) for i in range(N)] # 0: 'S', 1: 'I'
 
-    # workaround to make copies of the objects
-    state_nodes_clone = []
-    for e in state_nodes:
-        state_nodes_clone += [e.clone()]
+        current_time = 0
+        # print(state_nodes)
 
-    snapshots += [state_nodes_clone]
+        while current_time < 1000:
+            # pp.pprint(current_time)
+            node = state_nodes[0]
+            # sets the origin from the second node and so forth. Since there are only two nodes, the origin of the current
+            # node is always going to be the destiny of the previous node
+            node.origin = node.destiny
+            # if you are at state 'S'(0) you can only go to state 'I'(1)
+            current_time += node.dt
 
-#tests with the first individual of the population
-#TODO make this part generic for every individual
+            for i in range(1,N):
+                state_nodes[i].dt -= node.dt
 
-tS0 = 0
-tI0 = 0
-t = 0
+            if node.origin == 0:
+                node.destiny = 1
+                node.dt = inverse_exp_n(LAMBDA, GAMMA, get_number_of_infected(state_nodes))
+            # if you are at state 'I'(1) you can only go to state 'S'(0)
+            elif node.origin == 1:
+                node.destiny = 0
+                node.dt = inverse_exp(MU)
 
-# Output format
-# (State 1 -> State 2, time t)
-# An individual remains an amount of time t in the State 1 before transitioning to State 2
-print('Tests with the first individual of the population')
-for e in snapshots:
-    strBuff = ""
-    strBuff += str(e[0])
-    if e[0].origin == 0:
-        tS0 += e[0].dt
-    if e[0].origin == 1:
-        tI0 += e[0].dt
-    t += e[0].dt
-    print(strBuff)
+            state_nodes = sorted(state_nodes, key=lambda event: event.dt, reverse=False)
+            # pp.pprint(state_nodes)
 
-print("S0 {:.2f}% ".format(tS0*100 / t)) # percentage of time in which the first individual remains susceptible to infection
-print("I0 {:.2f}% ".format(tI0*100 / t)) # percentage of time in which the first individual is infected
+        # workaround to make copies of the objects
+        state_nodes_clone = []
+        for e in state_nodes:
+            state_nodes_clone += [e.clone()]
+
+        snapshots += [state_nodes_clone]
+
+        infected = []
+        healthy  = []
+        for s in snapshots:
+            number_of_infected = get_number_of_infected(s)
+            number_of_healthy  = len(s) - number_of_infected
+            infected += [number_of_infected/len(s)]
+            healthy  += [number_of_healthy/len(s)]
+            # print("S0 {:.2f}% ".format(number_of_healthy/len(s)*100)) # percentage of time in which the first individual remains susceptible to infection
+            # print("I0 {:.2f}% ".format(number_of_infected/len(s)*100)) # percentage of time in which the first individual is infected
+            # print()
+
+        def mean(_list):
+            return sum(_list)/len(_list)
+
+        print("S0 {:.2f}% ".format(mean(healthy)*100)) # percentage of time in which the first individual remains susceptible to infection
+        print("I0 {:.2f}% ".format(mean(infected)*100)) # percentage of time in which the first individual is infected
+
+simulation(N,GAMMA, MU, LAMBDA, EXPERIMENTS)
